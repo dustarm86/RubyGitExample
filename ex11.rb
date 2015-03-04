dd196e4193352ba57d6d84cbe4f97fcc63f8e9f1
Dustin <myownspiral9@gmail.com>
2015-03-04 15:42:48 -0500

    initial commit

---
+++ ex11.rb
@@ -0,0 +1,12 @@
+print "How old are you? " # comment about age
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "Where were you born? "
+born = gets.chomp
+print "Where did you go to high-school? "
+school = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, #{weight} ibs heavy, born in #{born}, and went to high-school at #{school}."
